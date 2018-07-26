decreased value iterations; fixed policy gradient calculation; adjust noise matrix for crazyflie

policy_network.py
@@ -25,7 +25,7 @@ class PolicyNet(NeuralNet):
             action_pred = self.model()
 
             # jacobian action wrt parameters
-            J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[:, idx], param)[0], [1, -1])
+            J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[:, idx].__getitem__(self.states_f_l), param), [1, -1])
                                                 for param in variables], 1) for idx in range(4)],
                                                 axis=1, name='jac_Action_wrt_Param')[0]
 
@@ -42,7 +42,7 @@ class PolicyNet(NeuralNet):
             position = 4 * 10**(-3) * tf.norm(position)
             angular = 3 * 10**(-4) * tf.norm(angular)
             linear = 5 * 10**(-4) * tf.norm(linear)
-            action = 2 * 10**(-4) * tf.norm(self.action_noise)
+            action = (2/3.) * 10**(-5) * tf.norm(self.action_noise)
 
             rf = position + action + angular + linear
 
@@ -78,8 +78,8 @@ class PolicyNet(NeuralNet):
             self.A = rf + DISCOUNT_VALUE * vf_w - vp_w
 
             # calculate gradients of A to and multiply with J (gradients of a to param)
-            grads = tf.gradients(self.A, action_pred)[0]
-            gk = tf.matmul([grads[1]], J)
+            grads = tf.gradients(self.A, action_pred)[0].__getitem__(self.states_f_l)
+            gk = tf.matmul([grads], J)
 
             # multiply gk with hessian pseudo inverse
             nk = tf.matmul(gk, h00_pinv)

policy_optimization_test.py
@@ -42,9 +42,10 @@ action_noise = tf.placeholder('float', [4])
 action_pred = policy_net.model()
 
 # jacobian action wrt parameters
-J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[:, idx], param)[0], [1, -1])
+J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[:, idx].__getitem__(states_f_l), param), [1, -1])
                                     for param in variables], 1) for idx in range(4)],
                                     axis=1, name='jac_Action_wrt_Param')[0]
+
 
 # svd for hessian pseudo inverse
 s_, U_, V_ = tf.svd(tf.matmul(tf.cholesky(tf.matrix_inverse(NOISE_COV)), J))
@@ -95,8 +96,8 @@ vp_w += DISCOUNT_VALUE**tf.cast(states_p_l, dtype='float') * value_p
 A = rf + DISCOUNT_VALUE * vf_w - vp_w
 
 # calculate gradients of A to and multiply with J (gradients of a to param)
-grads = tf.gradients(A, action_pred)[0]
-gk = tf.matmul([grads[1]], J)
+grads = tf.gradients(A, action_pred)[0].__getitem__(states_f_l)
+gk = tf.matmul([grads], J)
 
 # multiply gk with hessian pseudo inverse
 nk = tf.matmul(gk, h00_pinv)
@@ -113,7 +114,6 @@ with value_sess.as_default():
 
 with sess.as_default():
     param_update = 0
-    # Get states of branch (off policy states)
     states_f = np.random.rand(10,18)
     states_p = np.random.rand(10, 18)
 
@@ -130,7 +130,7 @@ with sess.as_default():
     noise = np.random.rand(4)*800 - 459
 
     # Feed the data to the optimization graph
-    _A, nk = sess.run([A, train_op], \
+    _A, nk, g, j, t = sess.run([A, train_op, grads, J, tf.gradients(A, [action_pred.__getitem__(T_f)])], \
             feed_dict={policy_net.input: states, \
             states_f_l: T_f, \
             states_p_l: T_p, \
@@ -138,5 +138,4 @@ with sess.as_default():
             action_noise: noise, \
             value_f: val_f, \
             value_p: val_p})
-    print(_A)
-
+    print(t)

qrl.py
@@ -117,6 +117,9 @@ def run_training(arguments):
             # Save prediction for the optimization step
             actions.append(action)
 
+            # Calculate and save cost of state
+            costs.append(cost(position, action, angular, linear))
+
             # Add PID controller outputs
             action[0] += -pitch_PID.output + yaw_PID.output
             action[1] += +roll_PID.output - yaw_PID.output
@@ -139,8 +142,6 @@ def run_training(arguments):
             # Save state vector
             states.append(state)
 
-            # Calculate and save cost of state
-            costs.append(cost(position, action, angular, linear))
 
         # Save initial trajectory to the list of the collection cycle
         trajectories.append({'level': -1, 'states': states, 'actions': actions, 'costs': costs})
@@ -187,6 +188,7 @@ def run_training(arguments):
 
                     # Save prediction for the optimization step
                     actions.append(action)
+                    _action = action
 
                     # Add PID controller outputs
                     action[0] += -pitch_PID.output + yaw_PID.output
@@ -215,7 +217,7 @@ def run_training(arguments):
                         states.append(state)
 
                         # Calculate and save cost of state
-                        costs.append(cost(position, action, angular, linear))
+                        costs.append(cost(position, _action, angular, linear))
 
                 # Save branch trajectory to the list of the collection cycle
                 trajectories.append({'level': n, 'noise': noise, 'states': states, 'actions': actions, 'costs': costs, 'position': b['position']})
@@ -257,7 +259,7 @@ def run_training(arguments):
                     loss = train_value_network(value_sess, value_net, value_traj)
                     if arguments.log:
                         value_log.write(str(loss)+'\n')
-                    if not i % 100:
+                    if not i % 10:
                         print('Value Loss:', loss)
                     if loss < VALUE_LOSS_LIMIT:
                         break;
@@ -309,7 +311,7 @@ def run_training(arguments):
 
                         states = np.concatenate((states_f, states_p))
                         junction = states_p[0]
-                        noise = trajectory['noise'] - 459
+                        noise = trajectory['noise'] - ACTION_BIAS
 
                         # Feed the data to the optimization graph
                         A, nk = policy_sess.run([policy_net.A, policy_net.train_op], \
@@ -427,7 +429,7 @@ def value_function(sess, value_net, costs, states, i):
 # Cost function as defined in formula 9
 def cost(position, action, angular, linear):
     position = 4 * 10**(-3) * np.linalg.norm(position)
-    action = 2 * 10**(-4) * np.linalg.norm(action)
+    action = (2/3.) * 10**(-5) * np.linalg.norm(action)
     angular = 3 * 10**(-4) * np.linalg.norm(angular)
     linear = 5 * 10**(-4) * np.linalg.norm(linear)
 

utils.py
@@ -3,7 +3,7 @@ from constants import *
 from random import sample
 
 def NOISE(): 
-    return np.diag(np.diag(np.random.normal(80, 10, 4)) + NOISE_COV)
+    return np.diag(np.diag(np.random.normal(0, ACTION_BIAS*NOISE_STDDEV, 4)) + NOISE_COV)
 
 def timer(x):
     hours, rem = divmod(x, 3600)