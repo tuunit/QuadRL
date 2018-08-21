added parallelized computation of branch trajectories. implemented adaptive learning rate. fixed value function training. added function for branch trajectory's visualization. seperated DroneInterface and Trajectory classes.

constants.py
@@ -1,18 +1,20 @@
 import numpy as np
 
 TRAJECTORIES_N = 256
-BRANCHES_N = 128
-INITIAL_LENGTH = 512
+BRANCHES_N = 64
+INITIAL_LENGTH = 1024
 BRANCH_LENGTH = int(512)
-NOISE_DEPTH = 2
-VALUE_ITERATIONS = 30
-VALUE_LOSS_LIMIT = 0.0001
+NOISE_DEPTH = 1
+VALUE_ITERATIONS = 200
+VALUE_LOSS_LIMIT = 0.
 DISCOUNT_VALUE = 0.99
-TIME_STEP = 0.02
+TIME_STEP = 0.01
 ACTION_BIAS = 18353
 ACTION_MAX = 30000
-NOISE_COV = np.matrix([ [5., 0., 0., 0.],
-                        [0., 5., 0., 0.],
-                        [0., 0., 5., 0.],
-                        [0., 0., 0., 5.]], dtype=np.float32)
+ACTION_SCALE = 100.
+NOISE_COV = np.matrix([[5., 0., 0., 0.],
+                       [0., 5., 0., 0.],
+                       [0., 0., 5., 0.],
+                       [0., 0., 0., 5.]], dtype=np.float32)
+CHOLESKY_COV = np.linalg.cholesky(NOISE_COV)
 NOISE_STDDEV = 0.02

drone_interface_icarus.py
@@ -2,56 +2,61 @@ import qsim
 from time import sleep
 from pyquaternion import Quaternion
 import numpy as np
-
+from queue import Queue
 
 class DroneInterface:
     """ Drone Publisher to control Gazebo via ROS and send data to the simulator
     """
+    orientation = [i < 4 for i in range(13)]
+    position = [i >= 4 and i < 7 for i in range(13)]
+    angular_velocity = [(i >= 7 and i < 10) for i in range(13)]
+    linear_velocity = [i >= 10 for i in range(13)]
+    dt = float()
 
-    def __init__(self):
-        self.orientation = [i < 4 for i in range(13)]
-        self.position = [i >= 4 and i < 7 for i in range(13)]
-        self.angular_velocity = [(i >= 7 and i < 10) for i in range(13)]
-        self.linear_velocity = [i >= 10 for i in range(13)]
+    @staticmethod
+    def init():
+        qsim.createSimulator()
 
-        self.pose = np.array([0.0 for _ in range(13)])
+    @staticmethod
+    def _get_pid(idx):
+        return qsim.getPIDState(idx)
 
-        self.dt = float()
-        qsim.init()
+    @staticmethod
+    def _set_pid(idx, pid):
+        qsim.setPIDState(pid, idx)
 
-    def reset(self):
-        self.pose = np.array([0.0 for _ in range(13)])
-        self.pose[self.orientation] = [1.0, 0.0, 0.0, 0.0]
-        self.pose[self.position][2] = 0.1
+    @staticmethod
+    def set_timestep(dt):
+        DroneInterface.dt = dt
 
-    def set_timestep(self, dt):
-        self.dt = dt
+    @staticmethod
+    def get_state(pose):
+        return pose[DroneInterface.orientation], pose[DroneInterface.position], \
+               pose[DroneInterface.angular_velocity], pose[DroneInterface.linear_velocity]
 
-    def set_pose(self, pose):
-        self.pose = np.array(pose)
+    @staticmethod
+    def release():
+        qsim.release()
+        DroneInterface.countSims = 0
 
-    def get_pose(self):
-        return self.pose.tolist()
-
-    def get_state(self):
-        return tuple(self.pose[self.orientation].tolist()), tuple(self.pose[self.position].tolist()), \
-               tuple(self.pose[self.angular_velocity].tolist()), tuple(self.pose[self.linear_velocity].tolist())
-
-    def update(self, thrusts):
-        thrusts = [x.item() for x in thrusts]
-        pose = qsim.update(self.pose.tolist(), thrusts, self.dt)
+    @staticmethod
+    def update_stateless(pose, pid, thrusts):
+        DroneInterface._set_pid(0, pid)
+        pose = qsim.update(pose.tolist(), thrusts.tolist(), DroneInterface.dt, 0)
+        pid = DroneInterface._get_pid(0)
         if pose is None:
             print(pose)
         else:
-            self.pose = np.array(pose)
-        #print(self.pose[self.position])
-        #sleep(0.005)
-        return self.get_pose()
+            pose = np.array(pose, dtype=np.float64)
+        return pose, pid
 
-    def initial_pose(self):
-        self.reset()
-        self.pose[self.position] = [np.random.normal(scale=3), np.random.normal(scale=3), np.random.rand() * 5 + 3]
-        #self.pose[self.position] = [0, 0, 5]
+    @staticmethod
+    def random_pose():
+        pose = np.array([0.0 for _ in range(13)])
+        pose[DroneInterface.orientation] = [1.0, 0.0, 0.0, 0.0]
+
+        pose[DroneInterface.position] = [np.random.normal(scale=3), np.random.normal(scale=3), np.random.rand() * 5 + 3]
+        pose[DroneInterface.position] = [0, 0, 5]
 
         x = np.random.normal(scale=20)
         y = np.random.normal(scale=20)
@@ -60,7 +65,58 @@ class DroneInterface:
         y = Quaternion(axis=[0, 1, 0], degrees=y)
         z = Quaternion(axis=[0, 0, 1], degrees=z)
         orientation = x * y * z
-        self.pose[self.orientation] = orientation.elements
+        #self.pose[DroneInterface.orientation] = orientation.elements
+        return np.float64(pose)
 
-    def __del__(self):
-        qsim.release()
+    @staticmethod
+    def get_pose_with_rotation_mat(pose):
+        orientation, position, angular, linear = DroneInterface.get_state(pose)
+        orientation = np.ndarray.flatten(Quaternion(orientation).rotation_matrix)
+        return np.concatenate((orientation, position, angular, linear))
+
+
+class Trajectory:
+
+    def __init__(self):
+        self.pose = DroneInterface.random_pose()
+        self.pid = [0.0 for _ in range(12)]
+
+    def reset(self):
+        self.pose = np.array([0.0 for _ in range(13)])
+        self.pose[DroneInterface.orientation] = [1.0, 0.0, 0.0, 0.0]
+        self.pose[DroneInterface.position][2] = 0.1
+        self.pose = np.float64(self.pose)
+
+    def set_pose(self, pose):
+        self.pose = np.array(pose)
+
+    def get_pose(self):
+        return self.pose
+
+    def set_pid(self, pid):
+        self.pid = pid
+
+    def get_pid(self):
+        return self.pid
+
+    def get_state(self):
+        return DroneInterface.get_state(self.pose)
+
+    def get_pose_with_rotation_mat(self):
+        return DroneInterface.get_pose_with_rotation_mat(self.pose)
+
+    def step(self, thrusts):
+        pose, pid = DroneInterface.update_stateless(self.pose, self.pid, thrusts)
+        if pose is None:
+            print(pose)
+        else:
+            self.pose = np.array(pose, dtype=np.float64)
+            self.pid = pid
+        return self.get_pose
+
+    def snapshot(self):
+        copy = Trajectory()
+        copy.set_pid(self.get_pid())
+        copy.set_pose(self.get_pose())
+        return copy
+

gui.py
@@ -8,45 +8,57 @@ class GUI():
         self.span = span
         self.fig = plt.figure()
         self.ax = Axes3D.Axes3D(self.fig)
-        self.ax.set_xlim3d([-10.0, 10.0])
+        self.ax.set_xlim3d([-5., 5.])
         self.ax.set_xlabel('X')
-        self.ax.set_ylim3d([-10.0, 10.0])
+        self.ax.set_ylim3d([-5., 5.])
         self.ax.set_ylabel('Y')
-        self.ax.set_zlim3d([-5, 15.0])
+        self.ax.set_zlim3d([-5., 5.])
         self.ax.set_zlabel('Z')
         self.ax.set_title('Quadcopter Simulation')
 
-        self.l1 = self.ax.plot([],[],[],color='blue',linewidth=3,antialiased=False)[0]
-        self.l2 = self.ax.plot([],[],[],color='red',linewidth=3,antialiased=False)[0]
-        self.hub = self.ax.plot([],[],[],marker='o',color='green', markersize=6,antialiased=False)[0]
+        l1 = self.ax.plot([], [], [], color='blue', linewidth=3, antialiased=False)[0]
+        l2 = self.ax.plot([], [], [], color='red', linewidth=3, antialiased=False)[0]
+        hub = self.ax.plot([], [], [], marker='o', color='green', markersize=6, antialiased=False)[0]
+        self.quadrotors = [{'l1': l1, 'l2': l2, 'hub': hub}]
 
         self.follow = False
         self.fig.canvas.mpl_connect('key_press_event', self.keypress)
 
-    def update(self, state):
+    def add_quadrotor(self):
+        l1 = self.ax.plot([], [], [], color='blue', linewidth=3, antialiased=False)[0]
+        l2 = self.ax.plot([], [], [], color='red', linewidth=3, antialiased=False)[0]
+        hub = self.ax.plot([], [], [], marker='o', color='green', markersize=6, antialiased=False)[0]
+        self.quadrotors.append({'l1': l1, 'l2': l2, 'hub': hub})
+
+    def update_batch(self, states, indices):
+        for i in indices:
+            self.update(states[i], i)
+
+    def update(self, state, idx):
         R = state['rotation_matrix']
         span = self.span
 
-        points = np.array([ [-span,0,0], [span,0,0], [0,-span,0], [0,span,0], [0,0,0], [0,0,0] ]).T
+        points = np.array([[-span, 0, 0], [span, 0, 0], [0, -span, 0], [0, span, 0], [0, 0, 0], [0, 0, 0]]).T
         points = np.dot(np.array([[0.70710678, -0.70710678, 0.],
                                [0.70710678,  0.70710678, 0.],
                                [0.        ,  0.        , 1.]]), points)
-        points = np.dot(R,points)
+        points = np.dot(R, points)
 
-        points[0,:] += state['position'][0]
-        points[1,:] += state['position'][1]
-        points[2,:] += state['position'][2]
+        points[0, :] += state['position'][0]
+        points[1, :] += state['position'][1]
+        points[2, :] += state['position'][2]
 
-        self.l1.set_data(points[0,0:2],points[1,0:2])
-        self.l1.set_3d_properties(points[2,0:2])
-        self.l2.set_data(points[0,2:4],points[1,2:4])
-        self.l2.set_3d_properties(points[2,2:4])
-        self.hub.set_data(points[0,5],points[1,5])
-        self.hub.set_3d_properties(points[2,5])
+        self.quadrotors[idx]['l1'].set_data(points[0,0:2],points[1,0:2])
+        self.quadrotors[idx]['l1'].set_3d_properties(points[2,0:2])
+        self.quadrotors[idx]['l2'].set_data(points[0,2:4],points[1,2:4])
+        self.quadrotors[idx]['l2'].set_3d_properties(points[2,2:4])
+        self.quadrotors[idx]['hub'].set_data(points[0,5],points[1,5])
+        self.quadrotors[idx]['hub'].set_3d_properties(points[2,5])
+
         if self.follow:
-            x = [points[0,5]-10, points[0,5]+10]
-            y = [points[1,5]-10, points[1,5]+10]
-            z = [points[2,5]-5, points[2,5]+15]
+            x = [points[0, 5]-10, points[0, 5]+10]
+            y = [points[1, 5]-10, points[1, 5]+10]
+            z = [points[2, 5]-5, points[2, 5]+15]
             self.ax.set_xlim3d(x)
             self.ax.set_ylim3d(y)
             self.ax.set_zlim3d(z)

neural_network.py
@@ -13,8 +13,8 @@ class NeuralNet:
         self.hls = len(self.shape) - 2
 
         with self.graph.as_default():
-            self.input = tf.placeholder('float', [None, self.shape[0]])
-            self.output = tf.placeholder('float')
+            self.input = tf.placeholder('float32', [None, self.shape[0]])
+            self.output = tf.placeholder('float32', [None, self.shape[-1]])
 
         self.weights = list()
         self.biases = list()
@@ -37,9 +37,9 @@ class NeuralNet:
             if not self.layers:
                 for i, wb in enumerate(zip(self.weights, self.biases)):
                     if i == 0:
-                        layer = tf.matmul(self.input, wb[0]) + wb[1]
+                        layer = tf.add(tf.matmul(self.input, wb[0]), wb[1])
                     else:
-                        layer = tf.matmul(self.layers[i-1], wb[0]) + wb[1]
+                        layer = tf.add(tf.matmul(self.layers[i-1], wb[0]), wb[1])
 
                     if i < self.hls:
                         layer = tf.nn.tanh(layer)

policy_network.py
@@ -12,95 +12,22 @@ class PolicyNet(NeuralNet):
             # initiliaze policy network variables
             variables = tf.trainable_variables()
 
-            # placeholder for optimizing of formula 5
-            self.states_f_l = tf.placeholder('int32')
-            self.states_p_l = tf.placeholder('int32')
-
-            self.value_f = tf.placeholder('float')
-            self.value_p = tf.placeholder('float')
-
-            self.junction_state = tf.placeholder('float', [18])
-            self.action_noise = tf.placeholder('float', [4])
-
+            self.action_grads = tf.placeholder('float32', [1, 4])
             action_pred = self.model()
 
             # jacobian action wrt parameters
-            J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[:, idx].__getitem__(self.states_f_l), param), [1, -1])
+            J = tf.stack([tf.concat([tf.reshape(tf.gradients(action_pred[0, idx], param), [1, -1])
                                                 for param in variables], 1) for idx in range(4)],
                                                 axis=1, name='jac_Action_wrt_Param')[0]
+
+            gk = tf.matmul(self.action_grads, J)
 
             # svd for hessian pseudo inverse
             s_, U_, V_ = tf.svd(tf.matmul(tf.transpose(tf.cholesky(tf.matrix_inverse(NOISE_COV))), J))
             h00_pinv = tf.matmul(tf.matmul(V_, tf.matrix_diag(tf.square(1/s_))), tf.transpose(V_))
 
-            # Calculate cost function for states_f_l+states_p_l current position but with noise as action
-            position = self.junction_state[9:12]
-            angular = self.junction_state[12:15]
-            linear = self.junction_state[15:18]
-            
-            position = 4. * 10**(-3) * tf.norm(position)
-            angular = 5. * 10**(-4) * tf.norm(angular)
-            linear = 5. * 10**(-4) * tf.norm(linear)
-            action = 1. * 10**(-4) * tf.norm(self.action_noise)
-
-            rf = position + action + angular + linear
-
-            # create value function for off policy
-            iterf = tf.constant(0, dtype=tf.int32)
-            vf = tf.constant(0, dtype=tf.float32)
-            cf = lambda i, vf: tf.less(i, self.states_f_l-1)
-
-            vf_w = tf.while_loop(cf, 
-                    self.gen_body(self.input[:self.states_f_l],
-                        action_pred, 
-                        0), 
-                    [iterf, vf])[1]
-            vf_w += DISCOUNT_VALUE**tf.cast(self.states_f_l-1, dtype='float') * self.value_f
-           
-            # create value function for on policy
-            iterp = tf.constant(0, dtype=tf.int32)
-            vp = tf.constant(0, dtype=tf.float32)
-            cp = lambda i, vp: tf.less(i, self.states_p_l-1)
-
-            vp_w = tf.while_loop(cp, 
-                    self.gen_body(self.input[self.states_f_l:],
-                        action_pred, 
-                        self.states_f_l), 
-                    [iterp, vp])[1]
-
-            vp_w += DISCOUNT_VALUE**tf.cast(self.states_p_l-1, dtype='float') * self.value_p
-
-            # sum up all itermediate results
-            self.A = rf + DISCOUNT_VALUE * vf_w - vp_w
-
-            def f1():
-                return 0.8*self.A
-
-            def f2():
-                return self.A
-            #self.A = tf.case({tf.less(0., self.A): f1}, default=f2)
-
-            factor = tf.square(tf.norm(self.action_noise - action_pred.__getitem__(self.states_f_l)))
-            # calculate gradients of A to and multiply with J (gradients of a to param)
-            grads = -tf.divide(tf.multiply(self.A, self.action_noise - action_pred.__getitem__(self.states_f_l)), factor)
-            gk = tf.matmul([grads], J)
-
             # multiply gk with hessian pseudo inverse
             nk = tf.matmul(h00_pinv, tf.transpose(gk))
-            self.train_op = tf.transpose(nk)
-            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=1)
-
-    # value function for gradient calculation in formula 5
-    def gen_body(self, states, actions, offset):
-        def body(i, v):
-            tmp = DISCOUNT_VALUE**tf.cast(i, dtype='float') 
-            position = 4. * 10**(-3) * tf.norm(states[i][9:12])
-            angular = 5. * 10**(-4) * tf.norm(states[i][12:15])
-            linear = 5. * 10**(-4) * tf.norm(states[i][15:18])
-            action = 1. * 10 ** (-4) * tf.norm(actions[i+offset])
-            cost = position + action + linear + angular
-            tmp = tmp * cost
-            v = v + tmp
-            v.set_shape(tf.TensorShape([]))
-            return i+1, v
-        return body
+            self.train_op = gk
+            self.beta = tf.sqrt(tf.constant(14000., dtype=tf.float32) / tf.matmul(gk, nk))
+            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.)

value_network.py
@@ -8,5 +8,5 @@ class ValueNet(NeuralNet):
         NeuralNet.__init__(self, shape, graph)
         with self.graph.as_default():
             self.loss = tf.reduce_mean(tf.losses.huber_loss(predictions=self.model(),labels=self.output))
-            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
             self.train_op = self.optimizer.minimize(self.loss)